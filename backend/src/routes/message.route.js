b4606cda34dd0f6e0e19597e1d950761b1752323
UMESH-SATPATHI <satpathiumesh.1917@gmail.com>
2026-08-27 21:07:51 +0530

    fix

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { getChatPartners, getAllContacts, getMessages, sendMessage } from "../controllers/message.controller.js";
-import { arcjetProtection } from "../lib/arcjet.js";
 
 const router = express.Router();
 
-router.use(arcjetProtection)
-router.use(protectRoute)
+router.use(protectRoute);
 
 router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);
